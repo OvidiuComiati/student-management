594efe5cb14585bc1d5e089f5ef4e8a5cc14600c
OvidiuComiati <44118481+OvidiuComiati@users.noreply.github.com>
2018-11-10 20:16:47 +0200

    Update const.js

--- config/const.js
+++ config/const.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB_URL: process.env.MONGODB_URI || 'mongodb://localhost:27017/DiabetApp',
+    DB_URL: process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAdmin',
     RANDOM_STRING: process.env.JWT_SECRET || 'abc123'
 }
